d793db4ae9a801ce37ae4d4096c1a52723aa7094
hj458377603 <hj458377603@hotmail.com>
2013-09-28 19:24:31 +0800

    衡俊：添加提示动画

---
+++ FindULib/UserControls/Toast.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media.Animation;
+
+namespace FindULib.UserControls
+{
+    public partial class Toast : UserControl
+    {
+        public event EventHandler ShowCompleted;
+
+        public Toast()
+        {
+            InitializeComponent();
+        }
+
+        public void Show(string text)
+        {
+            this.txtMessage.Text = text;
+            Storyboard storyBoard = (Storyboard)this.Resources["storyBoardWithHold"];
+            storyBoard.Begin();
+            storyBoard.Completed += new EventHandler(StoryBoard_Completed);
+        }
+
+        private void StoryBoard_Completed(object sender, EventArgs e)
+        {
+            ShowCompleted(sender, e);
+        }
+    }
+}
