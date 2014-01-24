43a3034df342d6cb7980dcbf36da60094b696bec
hj458377603 <hj458377603@hotmail.com>
2014-01-24 00:23:59 -0800

    jun:Cookie自动登陆

---
+++ 解析HtmlCookie登陆/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace 解析HtmlCookie登陆
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(747, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(60, 119);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(488, 403);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(562, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "登陆";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(702, 119);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(555, 403);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(547, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "读取本地Cookie登陆";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1306, 601);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 解析HtmlCookie登陆/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 解析HtmlCookie登陆
+{
+    public partial class Form1 : Form
+    {
+        public CookieContainer CC = new CookieContainer();
+
+        string url = "https://passport.cnblogs.com/login.aspx";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = HttpWebRequest.Create("https://passport.cnblogs.com/login.aspx") as HttpWebRequest;
+            request.Method = "POST";
+            request.Referer = "https://passport.cnblogs.com/login.aspx";
+            request.Host = "passport.cnblogs.com";
+
+            string param = "__EVENTTARGET=&__EVENTARGUMENT="
+                + "&__VIEWSTATE=/wEPDwULLTE1MzYzODg2NzZkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQtjaGtSZW1lbWJlcm1QYDyKKI9af4b67Mzq2xFaL9Bt"
+                + "&__EVENTVALIDATION=/wEdAAUyDI6H/s9f+ZALqNAA4PyUhI6Xi65hwcQ8/QoQCF8JIahXufbhIqPmwKf992GTkd0wq1PKp6+/1yNGng6H71Uxop4oRunf14dz2Zt2+QKDEIYpifFQj3yQiLk3eeHVQqcjiaAP"
+                + "&tbUserName=locas&tbPassword=hj1991120&btnLogin=登++录&txtReturnUrl=";
+
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(param);
+            request.ContentLength = data.Length;
+            request.CookieContainer = CC;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0";
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+            Stream stream = request.GetRequestStream();
+            stream.Write(data, 0, data.Length);
+            stream.Flush();
+            stream.Close();
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string res = reader.ReadToEnd();
+            this.richTextBox1.Text = res;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+            //myRequest.Method = "GET";
+            //myRequest.CookieContainer = CC;  //将COOKIE保存在CC
+            ////myRequest.Connection = "keep-alive";
+            //HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            //Stream vfimg = myResponse.GetResponseStream();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = "https://passport.cnblogs.com/login.aspx";
+            string indata = "__EVENTTARGET=&__EVENTARGUMENT="
+                + "&__VIEWSTATE=/wEPDwULLTE1MzYzODg2NzZkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQtjaGtSZW1lbWJlcm1QYDyKKI9af4b67Mzq2xFaL9Bt"
+                + "&__EVENTVALIDATION=/wEdAAUyDI6H/s9f+ZALqNAA4PyUhI6Xi65hwcQ8/QoQCF8JIahXufbhIqPmwKf992GTkd0wq1PKp6+/1yNGng6H71Uxop4oRunf14dz2Zt2+QKDEIYpifFQj3yQiLk3eeHVQqcjiaAP"
+                + "&tbUserName=locas&tbPassword=hj1991120&btnLogin=登++录&txtReturnUrl=http://www.cnblogs.com/";
+
+            url = "http://opac.njue.edu.cn/reader/redr_verify.php";
+            indata = "number=1120130547&passwd=152150&select=cert_no&returnUrl=";
+
+            url = "http://219.219.191.243:8081/m/reader/login.action;jsessionid=D01D26A71ACF073E5019798AAD14DD28";
+            indata = "number=1120130547&passwd=152150&type=1&user_login=登录";
+            string outdata = "";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(indata);
+
+            //新建一个HttpWebRequest 
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            //一定要设置ContentType
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            myHttpWebRequest.ContentLength = data.Length;
+            myHttpWebRequest.Method = "POST";
+
+            //新建一个CookieContainer来存放Cookie集合 
+            CookieContainer myCookieContainer = new CookieContainer();
+
+            //设置HttpWebRequest的CookieContainer为刚才建立的那个myCookieContainer 
+            myHttpWebRequest.CookieContainer = myCookieContainer;
+
+            //把数据写入HttpWebRequest的Request流 
+            Stream myRequestStream = myHttpWebRequest.GetRequestStream();
+            myRequestStream.Write(data, 0, data.Length);
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("utf-8"));
+
+            //关闭打开对象 
+            myRequestStream.Flush();
+            myRequestStream.Close();
+
+            //新建一个HttpWebResponse 
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+
+            //把数据从HttpWebResponse的Response流中读出 
+            Stream myResponseStream = myHttpWebResponse.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            outdata = myStreamReader.ReadToEnd();
+
+            //关闭打开的对象 
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            SaveCookie(myHttpWebRequest.CookieContainer);
+
+            ////显示"登录" 
+            ////拿到了Cookie，再进行请求就能直接读取到登录后的内容了 
+            //myHttpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            ////刚才那个CookieContainer已经存有了Cookie,把它附加到HttpWebRequest中则能直接通过验证 
+            //myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+
+            ////读取登陆后的内容
+            //myResponseStream = myHttpWebResponse.GetResponseStream();
+            //myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            //outdata = myStreamReader.ReadToEnd();
+
+            ////关闭打开的对象 
+            //myStreamReader.Close();
+            //myResponseStream.Close();
+
+            //Console.WriteLine(outdata);
+            //this.webBrowser1.DocumentText = outdata;
+        }
+
+
+        private void SaveCookie(object obj)
+        {
+            FileStream fileStream = File.Create("F://test.txt");
+            System.Runtime.Serialization.IFormatter formatter = new BinaryFormatter();
+
+            using (fileStream)
+            {
+                //这里就是进行序列化了
+                formatter.Serialize(fileStream, obj);
+            }
+        }
+
+        private T LoadCookie<T>() where T : class
+        {
+            System.Runtime.Serialization.IFormatter formatterTo = new BinaryFormatter();
+
+            //打开一个文件流
+            Stream stream = new FileStream("F://test.txt", FileMode.Open, FileAccess.Read);
+
+            using (stream)
+            {
+                //这里就是反进行序列化了
+                return (formatterTo.Deserialize(stream) as T);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string url = "http://219.219.191.243:8081/m/reader/login.action;jsessionid=D01D26A71ACF073E5019798AAD14DD28";
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            //一定要设置ContentType
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            myHttpWebRequest.Method = "POST";
+
+            myHttpWebRequest.CookieContainer = LoadCookie<CookieContainer>();
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            Stream myResponseStream = myHttpWebResponse.GetResponseStream();
+
+            StreamReader myStreamReader = new StreamReader(myHttpWebResponse.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+            string outdata = myStreamReader.ReadToEnd();
+
+            //关闭打开的对象 
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            Console.WriteLine(outdata);
+            this.webBrowser1.DocumentText = outdata;
+        }
+
+        private void Go(string str)
+        {
+            string url = string.Empty;
+
+            switch (str)
+            {
+                case "1":
+                    {
+                        url = "http://219.219.191.243:8081/m/reader/info.action;jsessionid=D01D26A71ACF073E5019798AAD14DD28";
+                        break;
+                    }
+                case "2":
+                    {
+                        url = "http://219.219.191.243:8081/m/reader/lend_list.action;jsessionid=D01D26A71ACF073E5019798AAD14DD28";
+                        break;
+                    }
+            }
+            GetInfo(url);
+        }
+
+        private void GetInfo(string url)
+        {
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            //一定要设置ContentType
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            myHttpWebRequest.Method = "POST";
+
+            myHttpWebRequest.CookieContainer = LoadCookie<CookieContainer>();
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            Stream myResponseStream = myHttpWebResponse.GetResponseStream();
+
+            StreamReader myStreamReader = new StreamReader(myHttpWebResponse.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+            string outdata = myStreamReader.ReadToEnd();
+
+            //关闭打开的对象 
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            Console.WriteLine(outdata);
+            this.webBrowser1.DocumentText = outdata;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Go(this.textBox2.Text);
+        }
+    }
+}
